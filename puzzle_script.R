6421741ba6b67531f72500e254efb65973a47f9e
Lilli Righter <lilli.righter@gmail.com>
2025-05-28 13:38:07 -0400

    fixed floor calculations and reporting solution function

--- puzzle_script.R
+++ puzzle_script.R
@@ -8,6 +8,19 @@ safecracker <- read_csv("safecracker.csv")
 colnames(safecracker) <- colnames(safecracker) %>% 
   str_replace_all(" ","_") #make the table have code-friendly column names
 
+
+check_solution <- function(constant=constant, w=w, x=x, y=y, z=z, posish=posish,num_turns_w=num_turns_w,num_turns_x=num_turns_x,num_turns_y=num_turns_y,num_turns_z=num_turns_z) {
+  summed_val <- constant + w + x + y + z
+  print(summed_val)
+  works<- if_else(summed_val == 50, "TRUE", "FALSE")
+  the_numbers <- c(constant,w,x,y,z)
+  print(the_numbers)
+  latest <- c(posish,num_turns_w,num_turns_x,num_turns_y,num_turns_z,works)
+  latest_df<-data.frame(matrix(latest, 1))
+  colnames(latest_df) <- c('Base_posish', 'num_turns_1', 'num_turns_2', 'num_turns_3', 'num_turns_4', 'works')
+  solutions_list<-ifelse(works=="TRUE", rbind(solutions_list,latest_df), solutions_df)
+  }
+
 ##initiate an empty dataframe where we will record the results of all possible solutions (this will be >1 million, 16^5)
 solutions_list <- data.frame(matrix(ncol = 6, nrow = 0))
 colnames(solutions_list) <- c('Base_posish', 'num_turns_1', 'num_turns_2', 'num_turns_3', 'num_turns_4', 'works')
@@ -22,15 +35,15 @@ for (i in 1:nrow(safecracker)) {
     rot_a <-h
     num_turns_w <- ifelse((rot_a>posish),(rot_a-posish), (rot_a + (16-posish)))
     for (J in 1:nrow(safecracker)) {
-      x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[posish]], safecracker$Wheel_2[[J]])
+      x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[h]], safecracker$Wheel_2[[J]])
       rot_b <-J
       num_turns_x <- ifelse((rot_b>posish),(rot_b-posish), (rot_b + (16-posish)))
       for (K in 1:nrow(safecracker)) {
-        y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[posish]], safecracker$Wheel_3[[K]])
+        y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[J]], safecracker$Wheel_3[[K]])
         rot_c <-K
         num_turns_y <- ifelse((rot_c>posish),(rot_c-posish), (rot_c + (16-posish)))
         for (L in 1:nrow(safecracker)) {
-          z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[posish]], safecracker$Wheel_4[[L]])
+          z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[K]], safecracker$Wheel_4[[L]])
           rot_d <-L
           num_turns_z <- ifelse((rot_d>posish),(rot_d-posish), (rot_d + (16-posish)))
     
@@ -49,33 +62,9 @@ for (i in 1:nrow(safecracker)) {
   }
 }
 
-circularize <-function(x) {ifelse(x<0, x+15, x)}
+
 
 ##now we want to get rid of the combinations that don't add up to 50 and we should be left with only possible rotations for each base column 
-solutions_that_work <- solutions_list %>%
-  filter(works=="TRUE") %>%
-  mutate_at(vars(2:5), 
-            ~as.numeric(.)) %>%
-  mutate_at(vars(2:5), ~circularize(.))
-
-base_1_solutions <- solutions_that_work %>%
-  filter(Base_posish == 1)
-
-base_2_solutions <- solutions_that_work %>%
-  filter(Base_posish == 2)
-
-base_3_solutions <- solutions_that_work %>%
-  filter(Base_posish == 3)
-
-base_4_solutions <- solutions_that_work %>%
-  filter(Base_posish == 4)
-
-base_5_solutions <- solutions_that_work %>%
-  filter(Base_posish == 5)
-
-runner <- inner_join(base_1_solutions, base_2_solutions, by = c("num_turns_1", "num_turns_2", "num_turns_3", "num_turns_4"))
-runner <- inner_join(runner, base_3_solutions, by =c("num_turns_1", "num_turns_2", "num_turns_3", "num_turns_4"))
-runner <- inner_join(runner, base_4_solutions, by = c("num_turns_1", "num_turns_2", "num_turns_3", "num_turns_4"))
 
 ##find the winner, which should be a combination that is present for all Base positions
 winner <- get_dupes(solutions_that_work, !Base_posish)
@@ -94,15 +83,15 @@ for (h in 1:nrow(safecracker)) {
   rot_a <-h
   num_turns_w <- ifelse((rot_a>posish),(rot_a-posish), (rot_a + (16-posish)))
   for (J in 1:nrow(safecracker)) {
-    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[posish]], safecracker$Wheel_2[[J]])
+    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[h]], safecracker$Wheel_2[[J]])
     rot_b <-J
     num_turns_x <- ifelse((rot_b>posish),(rot_b-posish), (rot_b + (16-posish)))
     for (K in 1:nrow(safecracker)) {
-      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[posish]], safecracker$Wheel_3[[K]])
+      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[J]], safecracker$Wheel_3[[K]])
       rot_c <-K
       num_turns_y <- ifelse((rot_c>posish),(rot_c-posish), (rot_c + (16-posish)))
       for (L in 1:nrow(safecracker)) {
-        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[posish]], safecracker$Wheel_4[[L]])
+        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[K]], safecracker$Wheel_4[[L]])
         rot_d <-L
         num_turns_z <- ifelse((rot_d>posish),(rot_d-posish), (rot_d + (16-posish)))
         
@@ -120,6 +109,11 @@ for (h in 1:nrow(safecracker)) {
   }
 }
 
+report_winner <- function(data) {
+  winning_solution <- data %>%
+    dplyr:: select("num_turns_1", "num_turns_2", "num_turns_3", "num_turns_4")
+  glue("From the starting position, turn Wheel 1: {winning_solution$num_turns_1} time(s), turn Wheel 2: {winning_solution$num_turns_2} time(s), turn Wheel 3: {winning_solution$num_turns_3} time(s), and turn Wheel 4: {winning_solution$num_turns_4} time(s).")
+}
 
 ##now we want to get rid of the combinations that don't add up to 50 and we should be left with only possible rotations for each base column 
 solutions_in_running <- solutions_list %>%
@@ -134,15 +128,15 @@ for (h in 1:nrow(safecracker)) {
   rot_a <-h
   num_turns_w <- ifelse((rot_a>posish),(rot_a-posish), (rot_a + (16-posish)))
   for (J in 1:nrow(safecracker)) {
-    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[posish]], safecracker$Wheel_2[[J]])
+    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[h]], safecracker$Wheel_2[[J]])
     rot_b <-J
     num_turns_x <- ifelse((rot_b>posish),(rot_b-posish), (rot_b + (16-posish)))
     for (K in 1:nrow(safecracker)) {
-      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[posish]], safecracker$Wheel_3[[K]])
+      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[J]], safecracker$Wheel_3[[K]])
       rot_c <-K
       num_turns_y <- ifelse((rot_c>posish),(rot_c-posish), (rot_c + (16-posish)))
       for (L in 1:nrow(safecracker)) {
-        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[posish]], safecracker$Wheel_4[[L]])
+        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[K]], safecracker$Wheel_4[[L]])
         rot_d <-L
         num_turns_z <- ifelse((rot_d>posish),(rot_d-posish), (rot_d + (16-posish)))
         
@@ -176,15 +170,15 @@ for (h in 1:nrow(safecracker)) {
   rot_a <-h
   num_turns_w <- ifelse((rot_a>posish),(rot_a-posish), (rot_a + (16-posish)))
   for (J in 1:nrow(safecracker)) {
-    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[posish]], safecracker$Wheel_2[[J]])
+    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[h]], safecracker$Wheel_2[[J]])
     rot_b <-J
     num_turns_x <- ifelse((rot_b>posish),(rot_b-posish), (rot_b + (16-posish)))
     for (K in 1:nrow(safecracker)) {
-      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[posish]], safecracker$Wheel_3[[K]])
+      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[J]], safecracker$Wheel_3[[K]])
       rot_c <-K
       num_turns_y <- ifelse((rot_c>posish),(rot_c-posish), (rot_c + (16-posish)))
       for (L in 1:nrow(safecracker)) {
-        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[posish]], safecracker$Wheel_4[[L]])
+        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[K]], safecracker$Wheel_4[[L]])
         rot_d <-L
         num_turns_z <- ifelse((rot_d>posish),(rot_d-posish), (rot_d + (16-posish)))
         
@@ -216,15 +210,15 @@ for (h in 1:nrow(safecracker)) {
   rot_a <-h
   num_turns_w <- ifelse((rot_a>posish),(rot_a-posish), (rot_a + (16-posish)))
   for (J in 1:nrow(safecracker)) {
-    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[posish]], safecracker$Wheel_2[[J]])
+    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[h]], safecracker$Wheel_2[[J]])
     rot_b <-J
     num_turns_x <- ifelse((rot_b>posish),(rot_b-posish), (rot_b + (16-posish)))
     for (K in 1:nrow(safecracker)) {
-      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[posish]], safecracker$Wheel_3[[K]])
+      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[J]], safecracker$Wheel_3[[K]])
       rot_c <-K
       num_turns_y <- ifelse((rot_c>posish),(rot_c-posish), (rot_c + (16-posish)))
       for (L in 1:nrow(safecracker)) {
-        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[posish]], safecracker$Wheel_4[[L]])
+        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[K]], safecracker$Wheel_4[[L]])
         rot_d <-L
         num_turns_z <- ifelse((rot_d>posish),(rot_d-posish), (rot_d + (16-posish)))
         
@@ -257,15 +251,15 @@ for (h in 1:nrow(safecracker)) {
   rot_a <-h
   num_turns_w <- ifelse((rot_a>posish),(rot_a-posish), (rot_a + (16-posish)))
   for (J in 1:nrow(safecracker)) {
-    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[posish]], safecracker$Wheel_2[[J]])
+    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[h]], safecracker$Wheel_2[[J]])
     rot_b <-J
     num_turns_x <- ifelse((rot_b>posish),(rot_b-posish), (rot_b + (16-posish)))
     for (K in 1:nrow(safecracker)) {
-      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[posish]], safecracker$Wheel_3[[K]])
+      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[J]], safecracker$Wheel_3[[K]])
       rot_c <-K
       num_turns_y <- ifelse((rot_c>posish),(rot_c-posish), (rot_c + (16-posish)))
       for (L in 1:nrow(safecracker)) {
-        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[posish]], safecracker$Wheel_4[[L]])
+        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[K]], safecracker$Wheel_4[[L]])
         rot_d <-L
         num_turns_z <- ifelse((rot_d>posish),(rot_d-posish), (rot_d + (16-posish)))
         
@@ -298,15 +292,15 @@ for (h in 1:nrow(safecracker)) {
   rot_a <-h
   num_turns_w <- ifelse((rot_a>posish),(rot_a-posish), (rot_a + (16-posish)))
   for (J in 1:nrow(safecracker)) {
-    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[posish]], safecracker$Wheel_2[[J]])
+    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[h]], safecracker$Wheel_2[[J]])
     rot_b <-J
     num_turns_x <- ifelse((rot_b>posish),(rot_b-posish), (rot_b + (16-posish)))
     for (K in 1:nrow(safecracker)) {
-      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[posish]], safecracker$Wheel_3[[K]])
+      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[J]], safecracker$Wheel_3[[K]])
       rot_c <-K
       num_turns_y <- ifelse((rot_c>posish),(rot_c-posish), (rot_c + (16-posish)))
       for (L in 1:nrow(safecracker)) {
-        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[posish]], safecracker$Wheel_4[[L]])
+        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[K]], safecracker$Wheel_4[[L]])
         rot_d <-L
         num_turns_z <- ifelse((rot_d>posish),(rot_d-posish), (rot_d + (16-posish)))
         
@@ -339,15 +333,15 @@ for (h in 1:nrow(safecracker)) {
   rot_a <-h
   num_turns_w <- ifelse((rot_a>posish),(rot_a-posish), (rot_a + (16-posish)))
   for (J in 1:nrow(safecracker)) {
-    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[posish]], safecracker$Wheel_2[[J]])
+    x <- ifelse(is.na(safecracker$Wheel_2[[J]]), safecracker$Floor_2[[h]], safecracker$Wheel_2[[J]])
     rot_b <-J
     num_turns_x <- ifelse((rot_b>posish),(rot_b-posish), (rot_b + (16-posish)))
     for (K in 1:nrow(safecracker)) {
-      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[posish]], safecracker$Wheel_3[[K]])
+      y <- ifelse(is.na(safecracker$Wheel_3[[K]]), safecracker$Floor_3[[J]], safecracker$Wheel_3[[K]])
       rot_c <-K
       num_turns_y <- ifelse((rot_c>posish),(rot_c-posish), (rot_c + (16-posish)))
       for (L in 1:nrow(safecracker)) {
-        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[posish]], safecracker$Wheel_4[[L]])
+        z <- ifelse(is.na(safecracker$Wheel_4[[L]]), safecracker$Floor_4[[K]], safecracker$Wheel_4[[L]])
         rot_d <-L
         num_turns_z <- ifelse((rot_d>posish),(rot_d-posish), (rot_d + (16-posish)))
         
@@ -371,6 +365,17 @@ st7 <-solutions_round_7 %>%
 s_run <- inner_join(st7, st_runn, by = c("num_turns_1", "num_turns_2", "num_turns_3", "num_turns_4", "works"))
 
 
+
+
+for (p in 1:nrow(solutions_that_work)) {
+  
+}
+
+
+
+
+
+
 #function test values
 posish<-3
 constant<-12
@@ -383,4 +388,31 @@ num_turns_x<-8
 num_turns_y<-1
 num_turns_z<-4
 
+####more garbage
+solutions_that_work <- solutions_list %>%
+  filter(works=="TRUE") %>%
+  mutate_at(vars(2:5), 
+            ~as.numeric(.)) %>%
+  mutate_at(vars(2:5), ~circularize(.))
+
+base_1_solutions <- solutions_that_work %>%
+  filter(Base_posish == 1)
+
+base_2_solutions <- solutions_that_work %>%
+  filter(Base_posish == 2)
+
+base_3_solutions <- solutions_that_work %>%
+  filter(Base_posish == 3)
+
+base_4_solutions <- solutions_that_work %>%
+  filter(Base_posish == 4)
+
+base_5_solutions <- solutions_that_work %>%
+  filter(Base_posish == 5)
+
+runner <- inner_join(base_1_solutions, base_2_solutions, by = c("num_turns_1", "num_turns_2", "num_turns_3", "num_turns_4"))
+runner <- inner_join(runner, base_3_solutions, by =c("num_turns_1", "num_turns_2", "num_turns_3", "num_turns_4"))
+runner <- inner_join(runner, base_4_solutions, by = c("num_turns_1", "num_turns_2", "num_turns_3", "num_turns_4"))
+
+circularize <-function(x) {ifelse(x<0, x+15, x)}
 ## a more elegant solution can prune answers as it goes 
